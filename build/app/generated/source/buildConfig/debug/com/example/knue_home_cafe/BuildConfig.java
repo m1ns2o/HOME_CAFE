10f5e59b2c1e957ce34ff2a1b419acfc316eaf51
m1ns2o <srjms0128@gmail.com>
2023-05-09 22:46:38 +0900

    future builder

---
+++ build/app/generated/source/buildConfig/debug/com/example/knue_home_cafe/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.knue_home_cafe;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.knue_home_cafe";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
